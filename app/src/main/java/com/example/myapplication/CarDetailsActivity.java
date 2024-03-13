64de7c9859735ee7e18771600e14bd38824cbc68
Osama Yasin <ausamaboss@gmail.com>
2024-03-13 23:53:01 +0300

    12:13 pm 13-3-2024

---
+++ app/src/main/java/com/example/myapplication/CarDetailsActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.myapplication.databinding.ActivityCarInfo1Binding;
+
+public class Car_info_1 extends AppCompatActivity {
+
+    private ActivityCarInfo1Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
